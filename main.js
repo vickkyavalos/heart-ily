952643992cac566ffdfe7d83b3ff2c0e690aaeb5
Victoria Inés Avalos <106405373+vickkyavalos@users.noreply.github.com>
2025-02-21 02:04:27 -0300

    html, css y js

---
+++ main.js
@@ -0,0 +1,7 @@
+let boton = document.getElementById('boton');
+let texto = document.getElementById('texto');
+
+boton.addEventListener('click', function() {
+    texto.style.display = 'block';
+});
+
